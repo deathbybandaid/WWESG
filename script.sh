d1954b93604f813a74e3857b9f9d9d8ed7e217a9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-14 15:37:48 -0400

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+## PRINT WINNER
+
+echo ""
+echo ""
+echo ""
+cat << "EOF"
+__          ____          ________  _____  _____
+ \ \        / /\ \        / /  ____|/ ____|/ ____|
+  \ \  /\  / /  \ \  /\  / /| |__  | (___ | |  __
+   \ \/  \/ /    \ \/  \/ / |  __|  \___ \| | |_ |
+    \  /\  /      \  /\  /  | |____ ____) | |__| |
+     \/  \/        \/  \/   |______|_____/ \_____|
+
+EOF
+echo ""
+echo ""
+echo ""
+echo "Importing Votes..."
+echo ""
+##
+echo ""
+echo "Processing Numbers..."
+echo ""
+##
+echo ""
+echo "Selecting Winner..."
+echo ""
+##
+echo ""
+echo "The winner is:"
+echo ""
+##
+echo "Becca"
+echo ""
+echo ""
